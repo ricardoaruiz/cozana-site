25388d504d752418a3bd58a7b405e7f68a8a3cf0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2023-06-01 18:45:53 -0300

    feat: update Description component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,16 @@ function App() {
     <>
       <Header />
       <Banner img={cozonac1} text="Făcut cu dragoste" />
-      <Description />
+      <Description
+        title="Despre"
+        text="Cozana Home este o brutărie artizanală unde se simte aroma cozonacilor
+          cu parfum de sarbatoare tot timpul anului, nu doar de sărbători. Se
+          plămădesc din ingrediente naturale, și se coc în tihnă. Indiferent
+          dacă sunt cu nucă, stafide, rahat sau mac sunt dulci, francezi de se
+          topesc în gură. Cozonacii se fac la comandă, după pofta clientului,
+          iar ingredientul secret , un stop de iubire născut din pasiunea pentru
+          dulciuri sănătoase."
+      />
       <Gallery />
       <Footer />
     </>
